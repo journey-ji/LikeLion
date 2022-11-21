10ae51581bf9aa907132d30a2e42a12ed0510510
journey-ji <journeyji95@gmail.com>
2022-11-21 09:11:41 +0900

    updated_TIL

---
+++ React/1114/my-app/src/components/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./test.module.css"
+
+const Test = () => {
+  return (
+    <section>
+      <h2>hello world</h2>
+      <p className={styles.one}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus, nemo!</p>
+    </section>
+  );
+};
+
+export default Test;
