9ce652bc6bff4496f3d5a0898d748d4786297a98
journey-ji <journeyji95@gmail.com>
2023-01-10 22:29:00 +0900

    책집필 예제 작성

--- book/src/App.jsx
+++ book/src/App.jsx
@@ -1,26 +1,60 @@
-import { createContext, useContext } from "react";
-import Jour from "./Jour";
-const UserInfo = createContext({
-  name: "재현",
-  age: "35",
-});
+import { useDebugValue, useEffect } from "react";
+import { useState } from "react";
+import "./style.css";
+
+const useBoxColorState = (initialValue, message) => {
+  const [value, setValue] = useState(initialValue);
+  useDebugValue(`${message}: ${value}`);
+  return [value, setValue];
+};
 function App() {
-  const { name, age } = useContext(UserInfo);
+  const [isColor, setIsColor] = useBoxColorState("", "현재 박스색상");
+  const [isTomato, setIsTomdato] = useState(false);
+  const [isPink, setIsPink] = useState(false);
+  const [isSky, setIsSky] = useState(false);
+
+  useEffect(() => {
+    if (isPink) {
+      setIsColor("pink");
+    }
+    if (isSky) {
+      setIsColor("sky");
+    }
+    if (isTomato) {
+      setIsColor("tomato");
+    }
+  }, [isTomato, isPink, isSky]);
   return (
-    <div>
-      <h2>{name}</h2>
-      <strong>{age}</strong>
-      <Jour />
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setIsTomdato(true);
+          setIsPink(false);
+          setIsSky(false);
+        }}
+      >
+        Tomato
+      </button>
+      <button
+        onClick={() => {
+          setIsTomdato(false);
+          setIsPink(true);
+          setIsSky(false);
+        }}
+      >
+        Pink
+      </button>
+      <button
+        onClick={() => {
+          setIsTomdato(false);
+          setIsPink(false);
+          setIsSky(true);
+        }}
+      >
+        Sky
+      </button>
+      <div className={isColor}></div>
+    </>
   );
 }
 export default App;
-
-/*
-음 ,,, 예제를 뭘로 하면 좋을까 ?
-영화관 ?
-각 층별 ,, 상영관 별 좌석 
-
-
-
-*/
--- book/src/context.jsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const UserInfo = createContext({
-  name: "창언",
-  age: "25",
-});
-
-export default UserInfo;
